d2d54bb0d7b87387d758ef70dca91d6d81da45b2
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-04-15 01:24:52 +0530

    add pop up in ToDoList.js

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CreateTask from '../modals/createTask';
 
 const ToDoList = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
     return (
         <>
             <div className = "header text-center">
                 <h1>Todo List</h1>
-                <button className = "btn btn-primary mt-2">Create Task</button>
+                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)}>Create Task</button>
             </div>
             <div className = "task-container">
 
             </div>
+            <CreateTask toggle = {toggle} modal ={modal}/>
         </>
     );
 };
