4056a446aa49ae9737e48fc9548079a12b5a8454
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-04-15 00:32:26 +0530

    add ToDoList component to App.js

---
+++ src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ToDoList = () => {
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <button>Create Task</button>
+        </div>
+    );
+};
+
+export default ToDoList;
