9480c69127aee8d1c345303f41df18eac8b2e848
Lakshya Kumar <lakshya.kumar_cs19@gla.ac.in>
2021-04-15 00:55:23 +0530

    add style to header in App.css

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 const ToDoList = () => {
     return (
-        <div>
-            <h1>Todo List</h1>
-            <button className = "btn btn-primary">Create Task</button>
-        </div>
+        <>
+            <div className = "header text-center">
+                <h1>Todo List</h1>
+                <button className = "btn btn-primary mt-2">Create Task</button>
+            </div>
+            <div className = "task-container">
+
+            </div>
+        </>
     );
 };
 
